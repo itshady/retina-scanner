a9522ff79c955ac7e41a79dd19b2e15fbb1b93f6
BaoGeist <linofbaoze@outlook.com>
2024-03-31 18:07:23 -0400

    integrated frontend and backend model; model.keras now needs to be in backend/ml

---
+++ backend/image_predictor.py
@@ -0,0 +1,39 @@
+import tensorflow as tf
+from tensorflow.keras.models import load_model
+from PIL import Image
+import numpy as np
+
+class ModelHandler():
+    def __init__(self):
+        save_path = 'ml/model.keras'
+        self.model = load_model(save_path)
+        self.labels = ['mild_npdr', 'moderate_npdr', 'nodr', 'pdr', 'severe_npdr']
+        
+
+    def predict(self, image):
+        """
+        This 
+
+        Parameters:
+        image (PIL.Image.Image): image to be predicted
+
+        Returns:
+        str: classification of image
+        """
+        # resize image for model
+        resized_image = image.resize((256, 256), Image.LANCZOS)
+
+        # get array from image
+        image_array = tf.keras.preprocessing.image.img_to_array(resized_image)
+
+        # get image_tensor to predict
+        image_tensor = tf.expand_dims(image_array, axis=0)
+
+        # predict category of image
+        res = self.model.predict(image_tensor)
+        predicted_classes = np.argmax(res, axis=1)
+        predicted_category = self.labels[predicted_classes[0]]
+
+        return predicted_category
+
+        
