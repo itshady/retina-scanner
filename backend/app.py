8fa0e9315163c799e1a1db9dd991ad4be218e7de
Baoze Lin <linofbaoze@outlook.com>
2024-04-01 14:19:25 -0400

    Model integration (#4)
    * ignore __pycache__ and the model.keras itself
    
    * integrated frontend and backend model; model.keras now needs to be in backend/ml
    
    * ignore __pycache__ and the model.keras itself
    
    * integrated frontend and backend model; model.keras now needs to be in backend/ml
    
    * Add touching off the screen
    
    ---------
    
    Co-authored-by: Hady Ibrahim <hadyib007@gmail.com>

--- backend/app.py
+++ backend/app.py
@@ -4,6 +4,8 @@ import base64
 from PIL import Image
 from io import BytesIO
 
+from image_predictor import ModelHandler
+
 app = Flask(__name__)
 CORS(app)
 
@@ -18,8 +20,12 @@ def upload_image():
     return jsonify({'message': 'No Image Received'}), 400
   image_data = data['image'].split(",")[1]  # Remove the prefix "data:image/jpeg;base64,"
   image = Image.open(BytesIO(base64.b64decode(image_data)))
+
+  modelHandler = ModelHandler()
+  res = modelHandler.predict(image)
+
   image.save('received_image.jpeg')  # Save or process the image
-  return jsonify({'message': 'Image received successfully'}), 200
+  return jsonify({'message': res}), 200
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=80)
--- frontend/App.js
+++ frontend/App.js
@@ -1,7 +1,5 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import LandingScreen from './screens/LandingScreen';
